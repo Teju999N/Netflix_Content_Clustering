dd98a1edf81738270d4e83c92f3782a2b7296761
TejaswiniCN24 <119392769+Teju999N@users.noreply.github.com>
2026-05-26 18:21:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+
+# Load model
+kmeans = pickle.load(open("model/kmeans.pkl", "rb"))
+tfidf = pickle.load(open("model/tfidf.pkl", "rb"))
+
+st.title("Netflix Content Clustering")
+
+user_input = st.text_input("Enter movie description")
+
+if user_input:
+    vec = tfidf.transform([user_input])
+    cluster = kmeans.predict(vec)
+
+    st.write("Cluster:", cluster)
